929d01d919f879515333b3637f900f9c33f3dc93
Saurabh Singh <90378786+SaurabhSinghRbz@users.noreply.github.com>
2022-10-13 19:34:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,46 +9,8 @@ const cors = require('cors');
 require('./middleware/auth');
 
 const app = express();
-// app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
 app.use(cors());
 app.use(express.json());
-
-// function isLoggedIn(req, res, next) {
-//     req.user ? next() : res.sendStatus(401);
-// }
-
-
-// app.get('/google', (req, res) => {
-//     res.send('<a href="/auth/google">Authenticate with Google</a>');
-// });
-
-// app.get('/auth/google',
-//     passport.authenticate('google', { scope: ['email', 'profile'] }
-//     ));
-
-// app.get('/auth/google/callback',
-//     passport.authenticate('google', {
-//         successRedirect: '/protected',
-//         failureRedirect: '/auth/google/failure'
-//     })
-// );
-
-// app.get('/http://localhost:3000/calender', isLoggedIn, (req, res) => {
-//     res.send(`Hello ${req.user.displayName}`);
-// });
-
-// app.get('/logout', (req, res) => {
-//     req.logout();
-//     req.session.destroy();
-//     res.send('Goodbye!');
-// });
-
-// app.get('/auth/google/failure', (req, res) => {
-//     res.send('Failed to authenticate..');
-// });
-
 
 
 app.get('/', (req, res) => {
@@ -67,7 +29,6 @@ app.get('/', (req, res) => {
         deleteTask: "http://localhost:8080/api/tasks/:id"
     });
 });
-
 
 
 
